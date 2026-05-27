436cecad292449488c9899a28ddc78b0b37e03f7
yonatbu-hue <yonatbu@ac.sce.ac.il>
2026-05-27 17:23:53 +0300

    Update normalized_array.py

--- normalized_array.py
+++ normalized_array.py
@@ -17,8 +17,13 @@ def normalized_array(data):
     data = np.array(data)
     
     # --- כיתבו את הקוד שלכם כאן ---
-    pass
-    # חשוב לזכור להחליף את pass ב- return
+    min_val = np.min(data)
+    max_val = np.max(data)
+    if max_val == min_val:
+        return np.zeros_like(data, dtype=float)
+    
+    normalized = (data - min_val) / (max_val - min_val)
+    return normalized
 
 if __name__ == "__main__":
     # כאן הסטודנטים יכולים להריץ בדיקה עצמית מהירה
